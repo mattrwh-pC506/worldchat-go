cab577f4136992070746da456478eb7e283a87dc
mattrwh-pC506 <mwhitt.w+ixgUOV9CjH10KUcF@gmail.com>
2023-04-22 20:50:48 -0700

    Setup basic React app and serve from go server

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, fmt.Sprintf("%v method not allowed", r.Method), http.StatusMethodNotAllowed)
 	}
-	http.ServeFile(w, r, "index.html")
+	http.ServeFile(w, r, "./chat-ui/build/index.html")
 }
 
 func main() {
@@ -29,9 +29,19 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", indexHandler)
+
+	fileServer := http.FileServer(http.Dir("./chat-ui/build"))
+	mux.Handle("/static/", fileServer)
+	mux.Handle("/manifest.json", fileServer)
+	mux.Handle("/robots.txt", fileServer)
+	mux.Handle("/favicon.ico", fileServer)
+	mux.Handle("/logo192.png", fileServer)
+	mux.Handle("/logo512.png", fileServer)
+
 	mux.HandleFunc("/authorize", func(w http.ResponseWriter, r *http.Request) {
 		authHandler(password, w, r)
 	})
+
 	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		chatHandler(room, w, r)
 	})
